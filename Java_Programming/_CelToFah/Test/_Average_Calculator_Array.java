9c3256deb6d1e7fa65d9e5dfc5e7d4ea31e98313
Sangmoo <first_1st@naver.com>
2018-07-17 16:07:32 +0900

    Calculator - Array

--- Java_Programming/_CelToFah/Test/_Average_Calculator_Array.java
+++ Java_Programming/_CelToFah/Test/_Average_Calculator_Array.java
@@ -7,19 +7,18 @@ public class _Average_Calculator_Array {
 	public static void main(String[] args) {
 		
 		int result = 0;
-		int[] s = new int[4];
+		int[] score = new int[4];
 		
 		System.out.println("수학, 영어, 물리, 프로그래밍 점수를 차례대로 입력하세요\n");
 		
 		Scanner input = new Scanner(System.in);
 		
-		for(int i=0; i < s.length; i++) {
-			s[i] = input.nextInt();
+		for(int i=0; i < score.length; i++) {
+			score[i] = input.nextInt();
+			result += score[i];
 		}
-		for(int j=0; j < s.length; j++) {
-			result += s[j];
-		}
-		result = result/s.length;
+		
+		result = result/score.length;
 		System.out.printf("모든 과목의 평균은 %d입니다.", result);
 		
 	}
