a506d1d048b1e7e326ac940b6fcf2272d00dc1b1
Sangmoo <first_1st@naver.com>
2018-07-17 11:11:20 +0900

    Grade - For

---
+++ Java_Programming/_CelToFah/Test/Average_Calculator_Grade_For.java
@@ -0,0 +1,34 @@
+package Test;
+
+import java.util.Scanner;
+
+public class Average_Calculator_Grade_For {
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		for(int i=0; i<100; i++) {
+			System.out.println("점수를 입력하세요 : ");
+			int score = input.nextInt();
+			
+			if(score >= 95) {
+				System.out.println("당신의 학점은 A+입니다.");
+			} else if(score >= 90 && score < 95) {
+				System.out.println("당신의 학점은 A입니다.");
+			} else if(score >= 85 && score < 90) {
+				System.out.println("당신의 학점은 B+입니다.");
+			} else if(score >= 80 && score < 85) {
+				System.out.println("당신의 학점은 B입니다.");
+			} else if(score >= 70 && score < 80) {
+				System.out.println("당신의 학점은 C입니다.");
+			} else if(score >= 60 && score < 70) {
+				System.out.println("당신의 학점은 D입니다.");
+			} else{
+				System.out.println("당신의 학점은 F입니다.");
+			}
+		}
+		
+	}
+
+}
