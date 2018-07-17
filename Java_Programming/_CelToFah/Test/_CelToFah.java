4be8d21c989eba9d1c5a050fb7824f39499dd85e
Sangmoo <first_1st@naver.com>
2018-07-17 15:52:55 +0900

    Cel To Fah

--- Java_Programming/_CelToFah/Test/_CelToFah.java
+++ Java_Programming/_CelToFah/Test/_CelToFah.java
@@ -6,31 +6,31 @@ public class _CelToFah {
 
 	public static void main(String[] args) {
 		
-		boolean b = true;
+		boolean flag = true;
 		
-		while(b) {
+		while(flag) {
 			
 			System.out.println("메뉴를 골라주세요\n1. 화씨 -> 섭씨\n2. 섭씨 -> 화씨");
 			
 			Scanner input = new Scanner(System.in);
-			int s = input.nextInt();
+			int select = input.nextInt();
 			
-			if(s == 1) {
-				Scanner inputF = new Scanner(System.in);
+			if(select == 1) {
+				Scanner inputFah = new Scanner(System.in);
 				System.out.print("화씨온도 입력 : ");
-				double f = inputF.nextDouble();
+				double fah = inputFah.nextDouble();
 			 
-				double c = (5.0 / 9.0) * (f - 32);
-				System.out.printf("화씨온도 %.3f는 섭씨온도 %.3f입니다.", f, c);
-				b = false;
-			} else if(s == 2) {
+				double cel = (5.0 / 9.0) * (fah - 32);
+				System.out.printf("화씨온도 %.3f는 섭씨온도 %.3f입니다.", fah, cel);
+				flag = false;
+			} else if(select == 2) {
 				Scanner inputC = new Scanner(System.in);
 				System.out.print("섭씨온도 입력 : ");
-				double c = inputC.nextDouble();
+				double cel = inputC.nextDouble();
 				
-	            double f = c * 1.8 + 32;
-	            System.out.printf("섭씨온도 %.3f는 화씨온도 %.3f입니다.", c, f);
-	            b = false;
+	            double fah = cel * 1.8 + 32;
+	            System.out.printf("섭씨온도 %.3f는 화씨온도 %.3f입니다.", cel, fah);
+	            flag = false;
 			} else {
 				System.out.println("메뉴를 1과 2중에 골라주세요\n");
 			}
