4db97ce59f8741a5b54870a881040b0b8cbf9ce0
Sangmoo <first_1st@naver.com>
2018-07-17 15:11:45 +0900

    Calculator

---
+++ Java_Programming/src/Hello/Calculator.java
@@ -0,0 +1,15 @@
+package Hello;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		
+		int one = 27;
+		int two = 26;
+		
+		int additionResult = one + two;
+		System.out.println(additionResult);
+
+	}
+
+}
