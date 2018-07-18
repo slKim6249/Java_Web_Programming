e15df771c723c2e8fe827f37ed0fa8855ba6b8fb
Sangmoo <first_1st@naver.com>
2018-07-18 09:40:41 +0900

    Document Comment

--- Java_Programming/src/Hello/VariableScope.java
+++ Java_Programming/src/Hello/VariableScope.java
@@ -1,12 +1,28 @@
 package Hello;
 
+/** Document Comment
+ * 변수의 종류와 범위를 실습하는 코드
+ * 
+ * @author Sangmoo ( first_1st@naver.com )
+ * @since 1.0
+ */
 public class VariableScope {
 
-	static int zipCode = 931220; // 멤버 변수
+	/**
+	 * 멤버변수 : 우편번호
+	 * */
+	static int zipCode = 931220;
 	
+	/**
+	 * 
+	 * Java Application의 시작점
+	 * 
+	 * @param args
+	 * @return void
+	 */
 	public static void main(String[] args) {
 		
-		int age = 26; // 지역 변수
+		int age = 26;
 		System.out.println(zipCode);
 		System.out.println(age);
 		
