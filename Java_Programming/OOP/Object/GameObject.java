51a961551931745c31f7b826e4522df9e27823e7
Sangmoo <first_1st@naver.com>
2018-07-24 13:31:30 +0900

    this, Set

--- Java_Programming/OOP/Object/GameObject.java
+++ Java_Programming/OOP/Object/GameObject.java
@@ -1,79 +1,129 @@
 package Object;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class GameObject {
 
-	   int healthPoint;
-	   int manaPoint;
-	   int damagePoint;
-	   
-	   List<String> healthPotionList;
-	   List<String> manaPotionList;
-	   
-	   /**
-	    * 피해간다
-	    */
-	   void run() {
-	      System.out.println("피해가기");
-	   }
-	   
-	   /**
-	    * 때린다 ( 일반 공격 )
-	    */
-	   void attack() {
-	      System.out.println(damagePoint + "만큼 공격합니다.");
-	   }
-	   
-	   /**
-	    * 때린다 ( 스킬 공격 )
-	    */
-	   int skillAttack() {
-	      System.out.println("마나를 사용해 스킬 공격을 합니다.");
-	      manaPoint = manaPoint - 10;
-	      return manaPoint;
-	   }
-	   
-	   /**
-	    * 맞는다
-	    */
-	   int beAttack ( int damage ) {
-	      System.out.println("상대에게 맞습니다.");
-	      healthPoint = healthPoint - damage;
-	      return healthPoint;
-	   }
-	   
-	   /**
-	    * MP 물약을 먹는다
-	    */
-	   void useManaPotion() {
-	      if ( manaPotionList.size() >  0) {
-	         manaPotionList.remove(0);
-	         manaPoint = manaPoint + 20;
-	      }
-	   }
-	   
-	   /**
-	    * HP 물약을 먹는다
-	    */
-	   void useHealthPotion() {
-	      if ( healthPotionList.size() > 0 ) {
-	         healthPotionList.remove(0);
-	         healthPoint = healthPoint + 20;
-	      }
-	   }
-	   
-	   /**
-	    * 죽었을 때, 아이템 하나를 떨어트린다
-	    */
-	   String dropItem() {
-	      if ( healthPotionList.size() > 0 ) {
-	         String healthPotion = healthPotionList.get(0);
-	         healthPotionList.remove(0);
-	         return healthPotion;
-	      }
-	      else {
-	         return "";
-	      }
-	   }
+	private int healthPoint;
+	private int manaPoint;
+	private int damagePoint;
+
+	private List<String> healthPotionList;
+	private List<String> manaPotionList;
+
+//	public void setHealthPoint(int healthPoint) {
+//
+//		if (healthPoint <= 10_000) {
+//			this.healthPoint = healthPoint;
+//		}
+//
+//	}
+//
+//	public void setManaPoint(int manaPoint) {
+//
+//		if (manaPoint <= healthPoint) {
+//			this.manaPoint = manaPoint;
+//		}
+//
+//	}
+
+	public int getDamagePoint() {
+		return damagePoint;
 	}
+
+	public void setDamagePoint(int damagePoint) {
+		this.damagePoint = damagePoint;
+	}
+
+	public List<String> getHealthPotionList() {
+		return healthPotionList;
+	}
+
+	public void setHealthPotionList(List<String> healthPotionList) {
+		this.healthPotionList = new ArrayList<String>();
+		this.healthPotionList.addAll(healthPotionList);
+	}
+
+	public List<String> getManaPotionList() {
+		return manaPotionList;
+	}
+
+	public void setManaPotionList(List<String> manaPotionList) {
+		this.manaPotionList = new ArrayList<String>();
+		this.manaPotionList.addAll(manaPotionList);
+	}
+
+	public int getHealthPoint() {
+		return healthPoint;
+	}
+
+	public int getManaPoint() {
+		return manaPoint;
+	}
+
+	/**
+	 * 피해간다
+	 */
+	void run() {
+		System.out.println("피해가기");
+	}
+
+	/**
+	 * 때린다 ( 일반 공격 )
+	 */
+	void attack() {
+		System.out.println(damagePoint + "만큼 공격합니다.");
+	}
+
+	/**
+	 * 때린다 ( 스킬 공격 )
+	 */
+	int skillAttack() {
+		System.out.println("마나를 사용해 스킬 공격을 합니다.");
+		manaPoint = manaPoint - 10;
+		return manaPoint;
+	}
+
+	/**
+	 * 맞는다
+	 */
+	int beAttack(int damage) {
+		System.out.println("상대에게 맞습니다.");
+		healthPoint = healthPoint - damage;
+		return healthPoint;
+	}
+
+	/**
+	 * MP 물약을 먹는다
+	 */
+	void useManaPotion() {
+		if (manaPotionList.size() > 0) {
+			manaPotionList.remove(0);
+			manaPoint = manaPoint + 20;
+		}
+	}
+
+	/**
+	 * HP 물약을 먹는다
+	 */
+	void useHealthPotion() {
+		if (healthPotionList.size() > 0) {
+			healthPotionList.remove(0);
+			healthPoint = healthPoint + 20;
+		}
+	}
+
+	/**
+	 * 죽었을 때, 아이템 하나를 떨어트린다
+	 */
+	String dropItem() {
+		if (healthPotionList.size() > 0) {
+			String healthPotion = healthPotionList.get(0);
+			healthPotionList.remove(0);
+			return healthPotion;
+		} else {
+			return "";
+		}
+	}
+}
--- Java_Programming/OOP/Object/GameObjectMain.java
+++ Java_Programming/OOP/Object/GameObjectMain.java
@@ -8,9 +8,6 @@ public class GameObjectMain {
    public static void main(String[] args) {
       
       GameObject character = new GameObject();
-      character.damagePoint = 30;
-      character.healthPoint = 1000;
-      character.manaPoint = 2000;
       
       List<String> characterHPList = new ArrayList<String>();
       characterHPList.add("HP물약");
@@ -24,26 +21,24 @@ public class GameObjectMain {
       characterMPList.add("MP물약");
       characterMPList.add("MP물약");
       
-      character.healthPotionList = characterHPList;
-      character.manaPotionList = characterMPList;
+      character.setDamagePoint(30);
+      character.setHealthPotionList(characterHPList);
+      character.setManaPotionList(characterMPList);
       
       // 몬스터
       GameObject monster = new GameObject();
-      monster.damagePoint = 10;
-      monster.healthPoint = 20;
-      monster.manaPoint = 0;
       
       List<String> monsterHPList = new ArrayList<String>();
       monsterHPList.add("몬스터 HP 물약");
       monsterHPList.add("몬스터 HP 물약");
       
-      monster.healthPotionList = monsterHPList;
-      monster.manaPotionList = new ArrayList<String>();
-      
+      monster.setDamagePoint(10);
+      monster.setHealthPotionList(monsterHPList);
+      monster.setManaPotionList(new ArrayList<String>());
       
       while ( true ) {
          monster.attack();
-         int characterHP = character.beAttack(monster.damagePoint);
+         int characterHP = character.beAttack(monster.getDamagePoint());
          
          if ( characterHP <= 0 ) {
             System.out.println("캐릭터가 사망하였습니다.");
@@ -56,7 +51,7 @@ public class GameObjectMain {
          }
          
          character.attack();
-         int monsterHP = monster.beAttack(character.damagePoint);
+         int monsterHP = monster.beAttack(character.getDamagePoint());
          
          if ( monsterHP <= 0 ) {
             System.out.println("몬스터가 사망했습니다.");
