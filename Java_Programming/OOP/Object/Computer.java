ec2cf75356ed557f98d72e61b7e5f1b214d15d95
Sangmoo <first_1st@naver.com>
2018-07-24 14:15:15 +0900

    Copy List

---
+++ Java_Programming/OOP/Object/Computer.java
@@ -0,0 +1,70 @@
+package Object;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Computer {
+
+	private String cpu;
+	private String gpu;
+	private List<String> ramList;
+	private List<String> hddList;
+	private List<String> ssdList;
+
+	public String getCpu() {
+		return cpu;
+	}
+
+	public void setCpu(String cpu) {
+		if( cpu.toLowerCase().startsWith("intel") ) {
+			this.cpu = cpu;
+		}
+	}
+
+	public String getGpu() {
+		return gpu;
+	}
+
+	public void setGpu(String gpu) {
+		if( gpu.toLowerCase().startsWith("nvdia") ) {
+			this.gpu = gpu;
+		}
+	}
+
+	public List<String> getRamList() {
+		List<String> ramListCopy = new ArrayList<String>();
+		ramListCopy.addAll(this.getRamList());
+		return ramListCopy;
+	}
+
+	public void setRamList(List<String> ramList) {
+		List<String> setRamList = new ArrayList<String>();
+		setRamList.addAll(ramList);
+		this.ramList = setRamList;
+	}
+
+	public List<String> getHddList() {
+		List<String> hddListCopy = new ArrayList<String>();
+		hddListCopy.addAll(this.getHddList());
+		return hddListCopy;
+	}
+
+	public void setHddList(List<String> hddList) {
+		List<String> setHddList = new ArrayList<String>();
+		setHddList.addAll(hddList);
+		this.hddList = setHddList;
+	}
+
+	public List<String> getSsdList() {
+		List<String> ssdListCopy = new ArrayList<String>();
+		ssdListCopy.addAll(this.getSsdList());
+		return ssdListCopy;
+	}
+
+	public void setSsdList(List<String> ssdList) {
+		List<String> setSsdList = new ArrayList<String>();
+		setSsdList.addAll(ssdList);
+		this.ssdList = setSsdList;
+	}
+
+}
