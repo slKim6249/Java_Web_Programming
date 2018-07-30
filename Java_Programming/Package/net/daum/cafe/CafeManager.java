c682a6f8d1389f52d5c24868bc2f7006deb8f121
Sangmoo <first_1st@naver.com>
2018-07-30 11:13:51 +0900

    Package

--- Java_Programming/Package/net/daum/cafe/CafeManager.java
+++ Java_Programming/Package/net/daum/cafe/CafeManager.java
@@ -1,6 +1,12 @@
 package net.daum.cafe;
 
 /**
+ * Package
+ * Ctrl + Shift + O 단축키
+ * java.lang은 import가 필요 없다.
+ * 대문자와 예약어를 쓸 수 없다.
+ * 소문자 영문자로만 쓰는게 낫다.
+ * . 으로 구분
  * 다른 패키지 사용
  */
 import net.daum.cafe.member.Cafe;
