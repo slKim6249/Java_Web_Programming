98f760b5f34df9f9737330e45d19506a56cbb23a
sangmoo <sangmoo@192.168.123.18>
2018-07-30 15:00:11 +0900

    Extends

---
+++ Java_Programming/Extends/com/ktds/Ktds.java
@@ -0,0 +1,43 @@
+package com.ktds;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Ktds {
+
+	public static void main(String[] args) {
+		
+		new Ktds().run();
+		
+	}
+	
+	public void run() {
+		
+		List<Friend> friendList = new ArrayList<Friend>();
+		
+		// Sun Class is a Super Class
+		// KtdsFriend is a Friend
+		
+		// SuperClass name = new SubClass();
+		Friend friend = new KtdsFriend();
+		
+		friend.setName("상무");
+		
+		if( friend instanceof KtdsFriend ) {
+			KtdsFriend ktFriend = (KtdsFriend) friend;
+			ktFriend.setPhone("010-8941-6248");
+			ktFriend.setAge(26);
+		}
+		
+		friendList.add(friend);
+		
+		friend = new Friend();
+		friend.setName("전상무");
+		friendList.add(friend);
+		
+		for (Friend f : friendList) {
+			System.out.println(f);
+		}
+	}
+
+}
---
+++ Java_Programming/Extends/com/ktds/KtdsFriend.java
@@ -0,0 +1,24 @@
+package com.ktds;
+
+public class KtdsFriend extends Friend {
+
+	private String phone;
+	private int age;
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+}
