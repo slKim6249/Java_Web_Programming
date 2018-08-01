244ecbdc3d36f8c5be723f2aa9a710d9fe52fd31
Sangmoo <first_1st@naver.com>
2018-08-01 10:42:37 +0900

    OJDBC

--- Java_Programming/HumanResource/com/ktds/HRDao.java
+++ Java_Programming/HumanResource/com/ktds/HRDao.java
@@ -5,11 +5,90 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import oracle.jdbc.proxy.annotation.GetCreator;
 
 public class HRDao {
 
 	public static void main(String[] args) {
-		new HRDao().run();
+//		new HRDao().run();
+		List<String> countryIdList = new ArrayList<String>();
+		countryIdList.add("JP");
+		countryIdList.add("CA");
+		countryIdList.add("US");
+		countryIdList.add("UK");
+		
+		// Java 9
+		List<String> cityList = List.of("JP", "CA", "US", "UK");
+		
+		new HRDao().getCities(cityList);
+	}
+	
+	public void getCities(List<String> countryIdList) {
+		
+		// 1. DB에 접근
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		try {
+			// Oracle 접속
+			conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE", 
+										"HR", "alqaeda7");
+			// 2. Query 준비 및 실행
+			pstmt = conn.prepareStatement("SELECT CITY FROM LOCATIONS"
+					+ "					WHERE IN (?, ?, ?, ?)"
+					+ "					AND STATE_PROVINCE IS NOT NULL"
+					+ "					ORDER BY COUNTRY_ID ASC, CITY DESC");
+			// Binding
+			pstmt.setString(1, countryIdList.get(0));
+			pstmt.setString(2, countryIdList.get(1));
+			pstmt.setString(3, countryIdList.get(2));
+			pstmt.setString(4, countryIdList.get(3));
+			
+			// 3. Query 결과 출력
+			rs = pstmt.executeQuery();
+			
+			// 결과 출력
+			while ( rs.next() ) {
+				String city = rs.getString("CITY");
+				System.out.println(city);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			// 연결을 하고 있다면
+			// DB 연결 해제 - DB를 연 역순으로 close
+			if ( rs != null ) {
+				try {
+					// DB 연결 해제
+					rs.close();
+				} catch (SQLException e) {} 
+			}
+			
+			if ( pstmt != null ) {
+				try {
+					// DB 연결 해제
+					pstmt.close();
+				} catch (SQLException e) {} 
+			}
+			
+			if ( conn != null ) {
+				try {
+					// DB 연결 해제
+					conn.close();
+				} catch (SQLException e) {} 
+			}
+		}
+		
 	}
 	
 	public void run() {
@@ -37,14 +116,12 @@ public class HRDao {
 								"jdbc:oracle:thin:@localhost:1521:XE" // DB에 접근하기 위한 url
 								, "HR"
 								, "alqaeda7");
-			
 			// 2. Query 준비 및 실행
-			
+
 			// Query만 적는다. 세미콜론 제외
 			// Query를 만들어서 DB한테 요청해주는 역할
 			pstmt = conn.prepareStatement("SELECT * FROM EMPLOYEES");
 			
-			// 3. Query 결과 출력
 			rs = pstmt.executeQuery();
 			
 			// 결과 출력
