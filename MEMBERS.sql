f33df844e8602356978b7e214292f1ee3f6fd482
Sangmoo <first_1st@naver.com>
2018-09-03 08:59:59 +0900

    MEMBERS Value

---
+++ MEMBERS.sql
@@ -0,0 +1,14 @@
+-- MEMBERS VALUE --
+Insert into SPRING.MEMBERS
+   (EMAIL, NAME, PASSWORD, POINT)
+ Values
+   ('ff@gmail.com', 'cossack', '123123', 68);
+Insert into SPRING.MEMBERS
+   (EMAIL, NAME, PASSWORD, POINT)
+ Values
+   ('hhh@hhh.com', 'HHH', 'hhh', 10);
+Insert into SPRING.MEMBERS
+   (EMAIL, NAME, PASSWORD, POINT)
+ Values
+   ('dkdidl@gmail.com', 'coskk', '456789', 22);
+COMMIT;
